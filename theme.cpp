b7ae5e60b2aac3b7608bd4f8f984b2ca6ad43101
Aaron J. Seigo <aseigo@kde.org>
2006-12-16 23:04:44 +0000

    get this to compile at least so i can start to exercise it with krunner and other such bits. no point on having this just sit on my disk i suppose =)
    svn path=/trunk/KDE/kdebase/workspace/plasma/lib/; revision=614239

---
+++ theme.cpp
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <KSharedConfig>
+#include <KStandardDirs>
+
+#include "theme.h"
+
+namespace Plasma
+{
+
+class Theme::Private
+{
+    public:
+        Private()
+            : themeName("default")
+        {
+        }
+
+    QString themeName;
+};
+
+Theme::Theme(QObject* parent)
+    : QObject(parent),
+      d(new Private)
+{
+    KConfig config("plasma");
+    KConfigGroup group(&config, "Theme");
+    d->themeName = group.readEntry("name", d->themeName);
+}
+
+Theme::~Theme()
+{
+}
+
+QString Theme::themeName() const
+{
+    return d->themeName;
+}
+
+QString Theme::imagePath(const QString& name)
+{
+    return KStandardDirs::locate("data", "desktoptheme/" + d->themeName
+                                          + "/" + name + ".svg");
+}
+
+}
+
+#include <theme.moc>
---
+++ theme.h
@@ -0,0 +1,49 @@
+/*
+ *   Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef PLASMA_THEME_H
+#define PLASMA_THEME_H
+
+#include <QtCore/QObject>
+
+namespace Plasma
+{
+
+class Theme : public QObject
+{
+    Q_OBJECT
+
+    public:
+        explicit Theme(QObject* parent = 0);
+        ~Theme();
+
+        QString themeName() const;
+        QString imagePath(const QString& name);
+
+    signals:
+        void changed();
+
+    private:
+        class Private;
+        Private* d;
+};
+
+} // Plasma namespace
+
+#endif // multiple inclusion guard
+
