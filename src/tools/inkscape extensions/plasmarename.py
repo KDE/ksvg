1d1f2920f8fd56a6d881ee74b5d66dc54551b124
Taro Tanaka <mkrmdk@gmail.com>
2023-10-29 13:04:31 +0900

    Update Inkscape extension for Inkscape >= 1.0
    Otherwise it won't work on the newer versions.
    
    Tested on Inkscape 1.3.
    
    See: https://inkscape.gitlab.io/extensions/documentation/authors/update1.0.html

--- src/tools/inkscape extensions/plasmarename.py
+++ src/tools/inkscape extensions/plasmarename.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 '''
 SPDX-FileCopyrightText: 2009 Marco Martin <notmart@gmail.com>
@@ -7,79 +6,60 @@ SPDX-FileCopyrightText: 2009 Marco Martin <notmart@gmail.com>
 SPDX-License-Identifier: GPL-2.0-or-later
 '''
 
-
-import sys
-sys.path.append('/usr/share/inkscape/extensions')
-
 import inkex
 
-from simplestyle import *
-from simpletransform import *
 import pathmodifier
+
 
 class PlasmaNamesEffect(pathmodifier.PathModifier):
     """
     Renames 9 selected elements as a plasma theme frame
     """
-    def __init__(self):
-        pathmodifier.PathModifier.__init__(self)
 
-        # Define string option "--prefix" with "-p" shortcut and default value "World".
-        self.OptionParser.add_option('-p', '--prefix', action = 'store',
-          type = 'string', dest = 'prefix', default = '',
-          help = 'Prefix of the svg elements')
-
-    def nodeBBox(self, node):
-        path = node
-        return computeBBox([path])
-
+    def add_arguments(self, pars):
+        pars.add_argument('--prefix', type=str, default='World', help='Prefix of the selected elements')
 
     def effect(self):
         # Get script's "--prefix" option value.
         prefix = self.options.prefix
 
-        #9 elements: is a frame. 4 elements: is a border hint
+        # 9 elements: is a frame. 4 elements: is a border hint
         positions = []
-        if len(self.selected) == 9:
+        if len(self.svg.selection) == 9:
             positions = ['topleft', 'left', 'bottomleft', 'top', 'center', 'bottom', 'topright', 'right', 'bottomright']
-        elif len(self.selected) == 4:
+        elif len(self.svg.selection) == 4:
             positions = ['hint-left-margin', 'hint-top-margin', 'hint-bottom-margin', 'hint-right-margin']
         else:
-            return
+            raise inkex.AbortExtension('This extension requires 4 or 9 selected elements.')
 
-        #some heuristics to normalize the values, find the least coords and size
+        # some heuristics to normalize the values, find the least coords and size
         minX = 9999
         minY = 9999
         minWidth = 9999
         minHeight = 9999
-        for id, node in self.selected.iteritems():
-            nodeBox = self.nodeBBox(node)
-            minX = min(minX, int(nodeBox[0]))
-            minY = min(minY, int(nodeBox[2]))
-            minWidth = min(minWidth, int(nodeBox[1] - nodeBox[0]))
-            minHeight = min(minHeight, int(nodeBox[3] - nodeBox[2]))
-
+        for node in self.svg.selection.values():
+            bbox = node.bounding_box()
+            minX = min(minX, bbox.x.minimum)
+            minY = min(minY, bbox.y.minimum)
+            minWidth = min(minWidth, bbox.width)
+            minHeight = min(minHeight, bbox.height)
 
         nodedictionary = {}
-        for id, node in self.selected.iteritems():
-            nodeBox = self.nodeBBox(node)
-            x = int(nodeBox[0])/minWidth - minX
-            y = int(nodeBox[2])/minHeight - minY
+        for node in self.svg.selection.values():
+            bbox = node.bounding_box()
+            x = bbox.x.minimum/minWidth - minX
+            y = bbox.y.minimum/minHeight - minY
             nodedictionary[x*1000 + y] = node
 
-        keys = nodedictionary.keys();
-        keys.sort();
         i = 0
-
-        for (k) in keys:
-            name = ''
+        for _, node in sorted(nodedictionary.items()):
             if prefix:
                 name = '%s-%s' % (prefix, positions[i])
             else:
                 name = '%s' % (positions[i])
-            nodedictionary[k].set('id', name)
-            i = i+1
+            node.set('id', name)
+            i += 1
 
-# Create effect instance and apply it.
-effect = PlasmaNamesEffect()
-effect.affect()
+
+if __name__ == '__main__':
+    PlasmaNamesEffect().run()
